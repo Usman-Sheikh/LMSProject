f435e584f6af154bcbd82a84c9d32b07a8c7958c
Usman-Sheikh <sh.usmanikram@hotmail.com>
2015-08-04 18:01:43 +0500

    Client

---
+++ LMS/LMS.WebClient/app/Students/MainController.js
@@ -0,0 +1,20 @@
+﻿
+
+
+    (function(){
+
+        var CustomerController=function($scope){
+    
+            $scope.sortBy = 'name';
+            $scope.reverse = false;
+            $scope.Customers = [{ name: 'Bilal', city: 'Lahore', joined: '20-03-2013', TotalOrder: '12' }, { name: 'Baber', city: 'Lala Musa', joined: '4-12-2013', TotalOrder: '13' }, { name: 'Asad', city: 'Pindi', joined: '24-06-2013', TotalOrder: '14' }, { name: 'Ali', city: 'Multan', joined: '10-07-2015', TotalOrder: '15' }]
+            $scope.dosort = function (propname) {
+                $scope.sortBy = propname;
+                $scope.reverse = !$scope.reverse;
+            };
+        };
+        CustomerController.$inject = ['$scope'];
+
+        angular.module('LMS')
+        .controller('CustomerController', CustomerController);
+}());
---
+++ LMS/LMS.WebClient/common/orderResource.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("orderResource",
+                ["$resource",
+                 "appSettings",
+                    orderResource])
+
+    function orderResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/Order", null,
+                {
+                    'update': { method: 'PUT' }
+                });
+    }
+}());
+
